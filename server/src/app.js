5fd624be00256b78228018d084c3600a9ba0d0ab
danielcampos <ddregalo@gmail.com>
2018-08-31 00:13:49 +0100

    Create get recipes route with dummy data example

--- server/src/app.js
+++ server/src/app.js
@@ -8,4 +8,18 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/recipes', (req, res) => {
+    res.send(
+      [{
+        title: "Lasagna",
+        category: "Dinner",
+        cuisine: "Italian",
+        description: "Vegetarian lasgna bake with tomatoe sauce and beschamel.",
+        totalTime: 60,
+        prepareTime: 25,
+        cookTime: 35
+      }]
+    )
+  })
+
 app.listen(process.env.PORT || 8081)
