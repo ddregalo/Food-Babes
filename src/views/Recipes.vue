51db74ffdf19600f27fc52aa9737e28b3e20b6c2
danielcampos <ddregalo@gmail.com>
2018-09-02 16:57:51 +0100

    Get recipes using RecipeService

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -14,25 +14,28 @@
 
 <script lang="ts">
     import Vue from "vue";
+    import RecipeService from "@/services/RecipeService.js"
     export default Vue.extend({
         props: [],
         data() {
             return {
-                // enthusiasm: this.initialEnthusiasm,
+                recipes: []
             }
         },
         methods: {
-            // increment() { this.enthusiasm++; },
-            // decrement() {
-            //     if (this.enthusiasm > 1) {
-            //         this.enthusiasm--;
-            //     }
-            // },
+            async getRecipes () {
+                const response = await RecipeService.getRecipes()
+                this.recipes = response.data
+            }
         },
         computed: {
             // exclamationMarks(): string {
             //     return Array(this.enthusiasm + 1).join('!');
             // }
+        },
+
+        created() {
+            this.getRecipes()
         }
     });
 </script>
@@ -46,3 +49,27 @@ h1 {
     color: #42b983;
 }
 </style>
+
+
+
+
+
+
+import PostsService from '@/services/PostsService'
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
