4c5332581ca8c0e2c5f99f64763c2b7ef6180ddf
danielcampos <ddregalo@gmail.com>
2018-09-02 15:54:09 +0100

    Create template page view for Recipes

---
+++ src/views/Recipes.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="recipes">
+        <div class="title">
+            <h1>recipes</h1>
+        </div>
+        <div v-for="recipe in recipes" :key="recipe.id">
+            <p>
+                <span><b>{{ recipe.title }}</b></span><br />
+                <span>{{ recipe.description }}</span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    export default Vue.extend({
+        props: [],
+        data() {
+            return {
+                // enthusiasm: this.initialEnthusiasm,
+            }
+        },
+        methods: {
+            // increment() { this.enthusiasm++; },
+            // decrement() {
+            //     if (this.enthusiasm > 1) {
+            //         this.enthusiasm--;
+            //     }
+            // },
+        },
+        computed: {
+            // exclamationMarks(): string {
+            //     return Array(this.enthusiasm + 1).join('!');
+            // }
+        }
+    });
+</script>
+
+<style>
+h1 {
+    margin-top: 0;
+    font-size: 100px;
+    letter-spacing: 5px;
+    text-transform: uppercase;
+    color: #42b983;
+}
+</style>
