af30c90555304ac4fdb2023354293de027d333b2
danielcampos <ddregalo@gmail.com>
2018-09-02 17:42:36 +0100

    Create recipe schema

---
+++ server/models/recipe.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var RecipeSchema = new Schema({
+    title: String,
+    description: String,
+    type: String,
+    cuisine: String,
+    totalTime: Number,
+    prepTime: Number,
+    cookTime: Number,
+    ingredients:
+        {
+            item: String,
+            quantity: Number
+        },
+    method: String
+});
+
+var Recipe = mongoose.model("Recipe", RecipeSchema);
+module.exports = Recipe;
