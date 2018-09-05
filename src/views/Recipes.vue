c142427c9868d0e0932b3cbd2a6624727ae8d4bf
danielcampos <ddregalo@gmail.com>
2018-09-05 22:41:33 +0100

    Show all recipes  + some styling

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="recipes">
         <div class="title">
-            <h1>recipes</h1>
+            <h1>recipes </h1>
+            <div class="new-recipe-link">
+                <router-link to="/recipes/new">New Recipe</router-link>
+            </div>
         </div>
-        <div v-for="recipe in recipes" :key="recipe.id">
+        <div v-for="recipe in recipes" :key="recipe._id">
             <p>
-                <span><b>{{ recipe.title }}</b></span><br />
-                <span>{{ recipe.description }}</span>
+                <span><b>{{ recipe.title }}</b></span><br/>
+                <span>{{ recipe.description }}</span><br/>
+                <span>{{ recipe.meal }}</span><br/>
+                <span>{{ recipe.cuisine }}</span><br/>
+                <span>{{ recipe.totalTime }}</span><br/>
+                <span>{{ recipe.prepTime }}</span><br/>
+                <span>{{ recipe.cookTime }}</span><br/>
+                <span>{{ recipe && recipe.ingredients && recipe.ingredients.item }}</span><br/>
+                <span>{{ recipe && recipe.ingredients && recipe.ingredients.quantity }}</span><br/>
+                <span>{{ recipe.method }}</span>
             </p>
         </div>
     </div>
@@ -14,28 +25,30 @@
 
 <script lang="ts">
     import Vue from "vue";
-    import RecipeService from "@/services/RecipeService.js"
+    import RecipeService from "../services/RecipeService";
+
     export default Vue.extend({
         props: [],
         data() {
             return {
-                recipes: []
+                recipes: [],
             }
         },
         methods: {
-            async getRecipes () {
-                const response = await RecipeService.getRecipes()
-                this.recipes = response.data
+            async getAllRecipes () {
+                try {
+                    const response = await RecipeService.getRecipes();
+                    return this.recipes = response.data.recipes;
+                }
+                catch (err)
+                {
+                    alert("Error Message Log: " + err)
+                }   
             }
         },
-        computed: {
-            // exclamationMarks(): string {
-            //     return Array(this.enthusiasm + 1).join('!');
-            // }
-        },
 
-        created() {
-            this.getRecipes()
+        mounted() {
+            this.getAllRecipes()
         }
     });
 </script>
@@ -43,33 +56,17 @@
 <style>
 h1 {
     margin-top: 0;
+    margin-bottom: 10px;
     font-size: 100px;
     letter-spacing: 5px;
     text-transform: uppercase;
     color: #42b983;
 }
-</style>
-
-
-
-
-
-
-import PostsService from '@/services/PostsService'
-export default {
-  name: 'posts',
-  data () {
-    return {
-      posts: []
-    }
-  },
-  mounted () {
-    this.getPosts()
-  },
-  methods: {
-    async getPosts () {
-      const response = await PostsService.fetchPosts()
-      this.posts = response.data
-    }
-  }
+p {
+    color: black;
 }
+
+.new-recipe-link {
+    margin: 50px;
+}
+</style>
