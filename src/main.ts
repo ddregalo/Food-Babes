788f5e06220799d6485b3d79bced49ce4a5551b2
danielcampos <ddregalo@gmail.com>
2018-09-05 23:02:47 +0100

    Add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import 'bootstrap';
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import'~bootstrap/dist/css/bootstrap.css';
 
 Vue.config.productionTip = false;
 
