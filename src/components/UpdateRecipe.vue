c74349753016dd590578ecf1e4f6ee267c6abea7
danielcampos <ddregalo@gmail.com>
2018-09-15 16:42:42 +0100

    Implement UpdateRecipeForm component

---
+++ src/components/UpdateRecipe.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="new-recipe">
+    <h1>Edit Recipe</h1>
+    <div class="container">
+        <UpdateRecipeForm></UpdateRecipeForm>
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+    import Vue from "vue";
+    import RecipeService from "@/services/RecipeService";
+    import UpdateRecipeForm from "@/components/UpdateRecipeForm.vue";
+    export default Vue.extend({
+    name: 'UpdateRecipe',
+    components: {
+        UpdateRecipeForm
+    }
+    })
+</script>
+
+<style type="text/scss">
+h1 {
+    font-size: 5em;
+    color: #4d7ef7;
+    margin-top: 0;
+    margin-bottom: 10px;
+    letter-spacing: 5px;
+    text-transform: uppercase;
+}
+.container {
+  width: 50%;
+}
+</style>
