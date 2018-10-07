015a3a2f437d1295e00cdf022263c4008421f18b
danielcampos <ddregalo@gmail.com>
2018-10-07 12:13:46 +0100

    E2e test homepage links

--- tests/e2e/specs/homepage.spec.js
+++ tests/e2e/specs/homepage.spec.js
@@ -1,8 +1,10 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("Homepage", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("Home");
+    cy.contains("About");
+    cy.contains("Recipes");
   });
 });
