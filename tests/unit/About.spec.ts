d55ea8b1497c20c95c2ed1b1151ed334b1fa7dfc
danielcampos <ddregalo@gmail.com>
2018-10-09 22:24:50 +0100

    Snapshot test

--- tests/unit/About.spec.ts
+++ tests/unit/About.spec.ts
@@ -1,10 +1,11 @@
-import { shallowMount } from '@vue/test-utils';
-const About = require('../../src/views/About');
+import { shallowMount } from "@vue/test-utils";
+const About = require("../../src/views/About");
 
-describe('About View', () => {
+describe("About View", () => {
   const wrapper = shallowMount(About);
 
-  it('renders the correct markup', () => {
-    expect(wrapper.html()).toContain('<h1>This is an about page</h1>');
+  it("renders the correct markup", () => {
+    expect(wrapper.html()).toContain("<h1>This is an about page</h1>");
+    expect(wrapper.html()).toMatchSnapshot();
   });
 });
