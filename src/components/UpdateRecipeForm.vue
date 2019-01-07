788f1efc47a6ed8c22894e467efaf102247dcb29
danielcampos <ddregalo@gmail.com>
2019-01-07 00:10:20 +0000

    Replace meals + cuisnes with vuex store

--- src/components/UpdateRecipeForm.vue
+++ src/components/UpdateRecipeForm.vue
@@ -60,8 +60,12 @@
 </template>
 
 <script type="ts">
-import Vue from "vue";
-import RecipeService from "@/services/RecipeService";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { store } from '@/store.ts';
+import RecipeService from '@/services/RecipeService';
+
+Vue.use(Vuex);
 
 export default Vue.extend({
   name: "UpdateRecipeForm",
@@ -80,28 +84,8 @@ export default Vue.extend({
         ingredients: [],
         method: ""
       },
-      meals: [
-        { text: "Select One", value: null },
-        "Breakfast",
-        "Lunch",
-        "Snack",
-        "Dinner",
-        "Desert"
-      ],
-      cuisines: [
-        { text: "Select One", value: null },
-        "American",
-        "Asian",
-        "British",
-        "Chinese",
-        "Desert",
-        "French",
-        "Indian",
-        "Italian",
-        "Mexican",
-        "Middle-Eastern",
-        "Other"
-      ]
+      meals: this.$store.state.meals,
+      cuisines: this.$store.state.cuisines,
     };
   },
   methods: {
